3eb5e8f6568806da569ee6fd8e2cc469391a237c
Daniela Prado <danipra2050@gmail.com>
2023-01-17 22:42:59 -0500

    Ejercicio tema 9

---
+++ ejercicio9/Cliente.java
@@ -0,0 +1,16 @@
+package com.co.bootcamp.ejercicio9;
+
+import com.co.bootcamp.ejercicio8.Persona;
+
+public class Cliente extends Persona {
+
+    private String credito;
+
+    public String getCredito() {
+        return credito;
+    }
+
+    public void setCredito(String credito) {
+        this.credito = credito;
+    }
+}
