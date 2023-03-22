bfc9394ffd4947693cebcfcee0211a313c7f94e2
Daniela Prado <danipra2050@gmail.com>
2023-03-22 11:28:29 -0500

    organized folders

---
+++ IntroProgramacion/ejercicio9/Trabajador.java
@@ -0,0 +1,15 @@
+package com.co.bootcamp.ejercicio9;
+
+import com.co.bootcamp.ejercicio8.Persona;
+
+public class Trabajador extends Persona {
+    private String salario;
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+}
--- ejercicio9/Persona.java
+++
@@ -1,8 +0,0 @@
-package com.co.bootcamp.ejercicio9;
-
-public class Persona {
-
-    private String nombre;
-    private int edad;
-    private long telefono;
-}
