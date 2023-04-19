14099c40d9dca34e94919a6b19b5faafc1018371
Daniela Prado <danipra2050@gmail.com>
2023-04-19 11:12:11 -0500

    Ejercicio 5

---
+++ Python/Ejercicio5.py
@@ -0,0 +1,11 @@
+def es_bisiesto(anio):
+    if anio % 4 == 0:
+        if anio % 100 == 0:
+            if anio % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
